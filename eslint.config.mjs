c289aa8f4c913fd5e3aef361ac2b16ef1962425a
Julian Whitley <jln.whitley@gmail.com>
2025-01-15 03:43:26 -0700

    updated eslint for dev env

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,13 +7,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['next'],
+    // extends: [
+    //   "next", 
+    //   "next/core-web-vitals", 
+    //   "next/typescript"
+    // ],
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-      
     },
   }),
 ]
